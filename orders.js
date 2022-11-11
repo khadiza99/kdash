eb630399c29895dfaf76213525391fa1043b62bd
khadiza99 <94781795+khadiza99@users.noreply.github.com>
2022-11-11 10:47:59 +0600

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,15 @@
+const Orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+  },
+
+  {
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined',
+  },
+];
